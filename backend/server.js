cfebc332e17c58ff70161f22a3e3b2f9d8d586f5
Jared Drake <jareddrake@Jareds-MacBook-Pro.local>
2021-05-04 16:28:02 -0600

    still broke

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//const uri = process.env.ATLAS_URI;
-mongoose.connect(process.env.ATLAS_URI, {
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri, {
   useNewUrlParser: true,
   useCreateIndex: true,
 });
