c542d87a6ea7eb272ee765dcb2435b0cd04023fd
Jared Drake <jareddrake@Jareds-MacBook-Pro.local>
2021-05-04 16:51:25 -0600

    still broke

--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -43,7 +43,7 @@ export default class ExercisesList extends Component {
   }
 
   deleteExercise(id) {
-    axios.delete("http://localhost:5000/exercises/" + id).then((response) => {
+    axios.delete("/exercises/" + id).then((response) => {
       console.log(response.data);
     });
 
